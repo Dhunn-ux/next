3269d6b167155d5a221da5fcc57a435abd10231c
Dhun-ux <sangeetkukreja26@gmail.com>
2023-08-11 21:11:47 +0530

    added
    navbar

---
+++ my-project/components/Banner.js
@@ -0,0 +1,10 @@
+// components/Banner.js
+
+export default function Banner() {
+    return (
+        <div className="bg-blue-500 text-white py-4 text-center">
+            <h1 className="text-2xl">Welcome to Our Website!</h1>
+            <p>Stay and explore what we offer.</p>
+        </div>
+    )
+}
---
+++ my-project/src/app/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+<body className="bg-gray-100">
+
+<div className="bg-white shadow-md">
+    <div className="container mx-auto px-6">
+        <nav className="flex justify-between items-center py-4">
+            <a href="#" className="text-xl font-bold text-gray-800">Logo</a>
+            <ul className="flex items-center space-x-4">
+                <li><a href="#" className="text-gray-600 hover:text-gray-800">Home</a></li>
+                <li><a href="#" className="text-gray-600 hover:text-gray-800">About</a></li>
+                <li><a href="#" className="text-gray-600 hover:text-gray-800">Services</a></li>
+                <li><a href="#" className="text-gray-600 hover:text-gray-800">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+
+</body>
+
